1ed8de38e286519ca73e72b10f79d097b05811ad
Abhishek Rawat <abhishekrawat708090@gmail.com>
2022-11-05 13:22:37 +0530

    created checkout page along with checkout items and checkout cart functionality

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -18,7 +18,7 @@ const ProductCard = ({ product }) => {
       <img src={imageUrl} alt={name} />
       <div className="footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">${price}</span>
       </div>
       <Button onClick={handleAddToCart} buttonType="inverted">
         Add to Cart
